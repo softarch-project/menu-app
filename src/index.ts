35bb80a5d29fbeafe202d1863018792e5485319e
tboonma <tboonma@hotmail.com>
2023-04-20 11:38:49 +0700

    Remove cors config

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,11 @@ import Constant from './constant'
 import HealthRouter from './routes/health.route'
 import ClientRouter from './routes/clientside.route'
 import RestaurantRouter from './routes/restaurant.route'
-import cors, { CorsOptions } from 'cors'
 
 const port = Constant.PORT
 const app: Express = express()
 const apiRouter = Router()
 
-apiRouter.use(cors())
 apiRouter.use('/restaurants', RestaurantRouter)
 apiRouter.use('/_z', HealthRouter)
 
